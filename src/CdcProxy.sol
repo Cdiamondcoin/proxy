cbaa426348ff1c27d6709e038eee2dd07de3a8ea
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2019-06-17 10:59:58 +0300

    Buy CDC via proxy contract

--- src/CdcProxy.sol
+++ src/CdcProxy.sol
@@ -1,33 +1,42 @@
 pragma solidity ^0.5.6;
 
+import "ds-proxy/proxy.sol";
+
+
 contract ERC20Interface {
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
     function transferFrom(address from, address to, uint tokens) public returns (bool success);
 }
 
 contract CdcExchangeInterface {
     function buyTokensWithFee() public payable returns (uint tokens);
-    function buyTokens() public payable returns (uint tokens);
-    function fee() public view returns (uint);
-
-    event LogBuyTokenWithFee(
-        address owner,
-        address sender,
-        uint ethValue,
-        uint cdcValue,
-        uint rate,
-        uint fee
-    );
 }
 
-// seth calldata "buy(address,address,address,address)" 0xb30041fF94fc8Fc071029F0ABc925A60B5a2059A 0x42aC13dA77bc7204b61C8a44Acba4411Fc34bbbf 0xe4e6e8aa984603e01685369ae9a204a55F17D4Ae 0x17645c5C8f208352B8cb02399Fa6B65630100C0B
 contract CdcProxy {
-    function buy(ERC20Interface dpt, ERC20Interface cdc, CdcExchangeInterface cdcex, address proxy) public payable {
-        uint fee = cdcex.fee();
-        dpt.transferFrom(msg.sender, proxy, fee);
-        dpt.approve(address(cdcex), fee);
-        uint tokens = cdcex.buyTokensWithFee.value(msg.value)();
-        cdc.transferFrom(proxy, msg.sender, tokens);
+    DSProxyFactory public factory;
+
+    constructor(DSProxyFactory factory_) public {
+        factory = factory_;
+    }
+
+    function buy(
+        ERC20Interface cdc,
+        CdcExchangeInterface cdcex
+    )
+        public payable returns (uint tokens)
+    {
+        tokens = cdcex.buyTokensWithFee.value(msg.value)();
+        cdc.transferFrom(address(this), msg.sender, tokens);
+        return tokens;
+    }
+
+    function createProxyAndBuy(
+        ERC20Interface cdc,
+        CdcExchangeInterface cdcex
+    )
+        public payable returns (uint tokens)
+    {
+        factory.build(msg.sender);
+        tokens = buy(cdc, cdcex);
+        return tokens;
     }
 }
--- src/CdcProxy.t.sol
+++ src/CdcProxy.t.sol
@@ -1,1 +1,24 @@
 pragma solidity ^0.5.6;
+
+import "ds-test/test.sol";
+import "ds-proxy/proxy.sol";
+import "./CdcProxy.sol";
+
+
+contract CdcProxyTest is DSTest {
+    DSProxyFactory factory;
+    CdcProxy proxy;
+
+    function setUp() public {
+        factory = new DSProxyFactory();
+        proxy = new CdcProxy(factory);
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
